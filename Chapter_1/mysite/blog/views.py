5351115a96a05d730126a159d6d558a06f89f696
romjanxr <romjanvr5@gmail.com>
2023-09-10 18:49:25 +0600

    Chapter 2 complete

--- Chapter_1/mysite/blog/views.py
+++ Chapter_1/mysite/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import get_object_or_404, render
-from blog.forms import EmailPostForm
+from blog.forms import CommentForm, EmailPostForm
 from blog.models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 # Create your views here.
 
 
@@ -45,7 +46,12 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                     'comments': comments,
+                                                     'form': form})
 
 
 def post_share(request, post_id):
@@ -71,3 +77,19 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
                                                     'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
